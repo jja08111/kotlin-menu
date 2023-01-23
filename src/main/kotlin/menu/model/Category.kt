7ca924f220084ff9c7b6397e4484fe6f9b2a06af
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-01-24 03:06:11 +0900

    feat(all): Enum 생성

---
+++ src/main/kotlin/menu/model/Category.kt
@@ -0,0 +1,33 @@
+package menu.model
+
+enum class Category(
+    val title: String,
+    val menus: List<String>
+) {
+    JAPANESE(
+        title = "일식",
+        menus = listOf("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")
+    ),
+    KOREAN(
+        title = "한식",
+        menus = listOf("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")
+    ),
+    CHINESE(
+        title = "중식",
+        menus = listOf("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")
+    ),
+    ASIAN(
+        title = "아시안",
+        menus = listOf("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")
+    ),
+    WESTERN(
+        title = "양식",
+        menus = listOf("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
+    )
+}
+
+val allMenus = Category.ASIAN.menus +
+        Category.CHINESE.menus +
+        Category.JAPANESE.menus +
+        Category.WESTERN.menus +
+        Category.KOREAN.menus
