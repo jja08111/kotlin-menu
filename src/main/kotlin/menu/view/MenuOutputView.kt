3ba3eb78fe5f1b813769f61f97a14e430078e3e6
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-01-24 17:29:29 +0900

    fix: 출력 줄바꿈 수정

--- src/main/kotlin/menu/view/MenuOutputView.kt
+++ src/main/kotlin/menu/view/MenuOutputView.kt
@@ -14,7 +14,7 @@ object MenuOutputView {
     }
 
     fun printInputCannotEatMenus(name: String) {
-        println("$name(이)가 못 먹는 메뉴를 입력해 주세요.")
+        println("\n$name(이)가 못 먹는 메뉴를 입력해 주세요.")
     }
 
     fun printResult(
@@ -22,13 +22,13 @@ object MenuOutputView {
         coaches: List<String>,
         suggestions: List<List<String>>
     ) {
-        println("메뉴 추천 결과입니다.")
+        println("\n메뉴 추천 결과입니다.")
         println("[ 구분 | ${Weekday.values().map { it.title }.makeTableRow()} ]")
         println("[ 카테고리 | ${categories.map { it.title }.makeTableRow()} ]")
         coaches.forEachIndexed { index, name ->
             println("[ $name | ${suggestions[index].makeTableRow()} ]")
         }
-        println("추천을 완료했습니다.")
+        println("\n추천을 완료했습니다.")
     }
 
     fun printError(message: String) {
