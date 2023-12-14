b1951efe7afd008120363db4deb0d9ac9da90654
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-12-15 00:36:29 +0900

    fix: add missing empty line

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -5,12 +5,14 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun readCoachNames(): List<String> {
+        println()
         println("코치의 이름을 입력해 주세요. (, 로 구분)")
         val line = Console.readLine()
         return line.split(",")
     }
 
     fun readUneatableMenus(coachName: String): List<String> {
+        println()
         println("${coachName}(이)가 못 먹는 메뉴를 입력해 주세요.")
         val line = Console.readLine()
         return line.split(",")
